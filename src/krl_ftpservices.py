7d5bba0b1de2d626ff6e3181572133acc93978e7
Krzysztof Szpieg <kszpieg@gmail.com>
2022-04-18 23:07:26 +0200

    Get list of files and folders from ftp server

--- src/krl_ftpservices.py
+++ src/krl_ftpservices.py
@@ -9,19 +9,39 @@ import wget as wget
 server = "54.38.52.41"
 rfactor_dir = ""
 ftp = ftplib.FTP(server)
+list_of_files = []
+list_of_folders = []
 
 
 def connect_to_ftp_server():
     ftp.login()
 
-    files = ftp.nlst(rfactor_dir)
-    print(files[0])
-    files1 = ftp.nlst(rfactor_dir + "/" + files[0])
-    print(files1)
-    files2 = ftp.nlst(rfactor_dir + files1[2])
-    print(files2)
-    files3 = ftp.nlst(files2[1])
-    print(files3)
+    # files = ftp.nlst(rfactor_dir)
+    # print(files)
+    # files1 = ftp.nlst(rfactor_dir + "/" + files[0])
+    # print(files1)
+    # files2 = ftp.nlst(files1[2])
+    # print(files2)
+    # files3 = ftp.nlst(files2[1])
+    # print(files3)
+    ftp_dir = ""
+    files = ftp.nlst(ftp_dir)
+    temp_path = ftp_dir + "/" + files[0]
+    temp_list = create_list_of_files(temp_path)
+    print("Files: " + str(temp_list[0]))
+    print("Folders: " + str(temp_list[1]))
+
+
+def create_list_of_files(path):
+    files = ftp.nlst(path)
+    for file in files:
+        if file[-4] == ".":
+            list_of_files.append(file)
+        else:
+            list_of_folders.append(file)
+            create_list_of_files(file)
+
+    return list_of_files, list_of_folders
 
 
 def download_files():
