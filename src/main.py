6b5f9b88c8907b0c22970d6c813e637f383b1bc6
Krzysztof Szpieg <kszpieg@gmail.com>
2022-04-18 14:13:38 +0200

    Add Panel with buttons and static box

--- src/main.py
+++ src/main.py
@@ -1,11 +1,46 @@
 import wx
 
+
+class MainPanel(wx.Panel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        btn_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        status_box = wx.StaticBox(self, wx.ID_ANY, "Status:")
+        status_box_sizer = wx.StaticBoxSizer(status_box, wx.VERTICAL)
+
+        btn_data = [("Check updates", btn_sizer, self.check_updates),
+                    ("Download files", btn_sizer, self.download_files)]
+        for data in btn_data:
+            label, sizer, handler = data
+            self.btn_builder(label, sizer, handler)
+
+        self.status_text = wx.StaticText(status_box, wx.ID_ANY, label="It's works!", style=wx.ALIGN_CENTER)
+        status_box_sizer.Add(self.status_text, wx.ID_ANY, wx.TOP, 10)
+
+        main_sizer.Add(status_box_sizer, wx.ALL | wx.EXPAND | wx.ALIGN_TOP, 5)
+        main_sizer.Add(btn_sizer, wx.ALIGN_BOTTOM, 5)
+        self.SetSizer(main_sizer)
+
+    def btn_builder(self, label, sizer, handler):
+        btn = wx.Button(self, label=label)
+        btn.Bind(wx.EVT_BUTTON, handler)
+        sizer.Add(btn, 0, wx.ALL | wx.CENTER, 5)
+
+    def check_updates(self, event):
+        self.status_text.SetLabel("check updates btn")
+
+    def download_files(self, event):
+        self.status_text.SetLabel("download files btn")
+
+
 class MainFrame(wx.Frame):
     def __init__(self):
         super().__init__(parent=None, title="KRL Updater")
+        self.panel = MainPanel(self)
         screensize = wx.DisplaySize()
-        w = screensize[0] * 0.4
-        h = screensize[1] * 0.3
+        w = screensize[0] * 0.15
+        h = screensize[1] * 0.05
         self.SetMinSize((w, h))
         self.Show()
 
